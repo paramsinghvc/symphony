cd69315588061de87b84b48124de7e4f6d9d4b15
Parminder Singh <paramsinghvc@gmail.com>
2016-02-08 14:30:55 +0530

    Gal mithi mithi bol

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,22 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     babelify = require('babelify'),
     source = require('vinyl-source-stream'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
+    browserSync = require('browser-sync'),    
     sass = require('gulp-sass'),
     plumber = require('gulp-plumber'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    reload = browserSync.reload;
 
 gulp.task('sass', function() {
     return gulp.src('assets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('build.css'))
         .pipe(gulp.dest('assets/css'))
+        .pipe(connect.reload())
 });
+
+gulp.task('sass:watch', ['sass']);
 
 gulp.task('transpile', function() {
     return browserify({
@@ -28,14 +32,30 @@ gulp.task('transpile', function() {
         .bundle()
         .pipe(source('index.js'))
         .pipe(gulp.dest('app/build'))
+        .pipe(connect.reload())
 });
 
-gulp.task('transpile:watch', ['transpile'], reload);
+gulp.task('transpile:watch', ['transpile']);
 
 gulp.task('serve', function() {
     connect.server({
         root: __dirname,
         port: process.env.PORT || 5000,
-        livereload: false
+        livereload: true
     });
-})
+});
+
+gulp.task('default', ['transpile', 'sass', 'serve'], function() {
+    // browserSync.init({
+    //     server: {
+    //         baseDir: "./"
+    //     }
+    // });
+    gulp.watch(['app/src/**/*.js'], ['transpile:watch']);
+    gulp.watch(['assets/sass/**/*.scss'], ['sass:watch']);
+});
+
+
+
+
+
